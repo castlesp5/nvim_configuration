d36f0a13d44637887062687daf68247cb7763111
Ismail Boujdad <iboujdad@c1r4p12.1337.ma>
2026-08-07 13:11:43 +0100

    tabs configurations

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+-- -- -- -- -- -- -- -- -- -- -- --
+--  Welcome to my humble config  --
+--            GitHub:            --
+-- https://github.com/castlesp5/ --
+-- -- -- -- -- -- -- -- -- -- -- --
+
 require("config.keymaps")
 require("config.options")
 require("config.lazy")
