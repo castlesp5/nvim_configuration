9daaf7ac5af43a05f471068c0c94705cb0781411
Ismail Boujdad <iboujdad@c1r4p12.1337.ma>
2026-08-07 13:09:07 +0100

    tabs configurations

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,16 @@
-vim.keymap.set('n', ';', ':')
-vim.keymap.set('n', '<C-s>', ':w<CR>')
+-- This File has only keymaps --
+
+
+
+-- shortcuts --
+vim.keymap.set('n', ';', ':') -- mapping ; to :
+vim.keymap.set('n', '<C-s>', ':w<CR>') -- saves with <CTRL + S>
+
+-- navigate your tabs -- 
+vim.keymap.set('n', '<C-n>', ':tabnew<CR>') -- creates a new empty tab
+vim.keymap.set('n', 'x', ':q<CR>') -- close a tab 
+vim.keymap.set('n', '<C-Left>', ':tabp<CR>') -- switching tabs
+vim.keymap.set('n', '<C-Right>', ':tabn<CR>') -- switching tabs
+vim.keymap.set('n', '<C-h>', ':tabp<CR>') -- switching tabs
+vim.keymap.set('n', '<C-l>', ':tabn<CR>') -- switching tabs
+vim.keymap.set('n', '<Tab>', ':tabn<CR>') -- switching tabs
---
+++ lua/config/options.lua
@@ -0,0 +1,9 @@
+local opt = vim.opt
+
+opt.clipboard = "unnamedplus"       -- Sync with system clipboard
+opt.mouse = "a"                     -- Enable mouse support
+opt.number = true                   -- Show line numbers
+opt.numberwidth = 4                 -- Number column width
+opt.termguicolors = true            -- True color support
+opt.splitbelow = true               -- Horizontal splits go to bottom
+opt.splitright = true               -- Vertical splits go to the right
